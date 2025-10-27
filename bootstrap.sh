e751a63ac9c0660c2ae6103834d522585107e401
Eirik Bye Hauge <eirik.bye.hauge@adresseavisen.no>
2025-10-27 15:47:22 +0100

    fix: ingressclassname fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,16 +3,17 @@
 set -euo pipefail
 
 # Install argocd from raw manifest
-kubectl apply -f manifest/bootstrap/demo/argocd/argocd.yaml
+microk8s kubectl apply -f manifest/bootstrap/demo/argocd/argocd.yaml
 
 # Wait for argocd server to be ready
 echo "Waiting for ArgoCD crds..."
-kubectl wait --for=condition=Established --timeout=180s crd/applications.argoproj.io crd/appprojects.argoproj.io
+microk8s kubectl wait --for=condition=Established --timeout=180s crd/applications.argoproj.io crd/appprojects.argoproj.io
 
 # Install applications
 echo "Install applications..."
-kubectl apply -f manifest/bootstrap/demo/applications/applications.yaml
+microk8s kubectl apply -f manifest/bootstrap/demo/applications/applications.yaml
 
 # Print admin password
+sleep 5  # Wait for secret to be created
 echo "ArgoCD initial admin password: "
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+microk8s kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
